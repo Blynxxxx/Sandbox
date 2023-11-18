2db7dc767199e77c2ca331d68f59e27f9e46bf37
linhvan <baolinh.van@my.jcu.edu.au>
2023-11-18 16:35:14 +0800

    Check directories

--- password_stars.py
+++ password_stars.py
@@ -1,2 +1,7 @@
+MINIMUM_LENGTH = 10
+
 password = input("Input your password: ")
+while len(password) < MINIMUM_LENGTH:
+    print("invalid password")
+    password = input("Input your password: ")
 print("*" * len(password))
